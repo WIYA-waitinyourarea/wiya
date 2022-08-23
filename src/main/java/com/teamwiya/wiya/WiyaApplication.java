23ad52b0e752dcd9cbe41f57b9f00d14f7e1cd0c
두부쿠키 <bkh3030@gmail.com>
2022-08-23 23:52:45 +0900

    Initial commit

---
+++ src/main/java/com/teamwiya/wiya/WiyaApplication.java
@@ -0,0 +1,13 @@
+package com.teamwiya.wiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiyaApplication.class, args);
+	}
+
+}
