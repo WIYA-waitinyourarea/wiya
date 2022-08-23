ea34194598cd90d99af5699eadb199fd41adf116
두부쿠키 <bkh3030@gmail.com>
2022-08-23 23:52:45 +0900

    Initial commit

---
+++ src/test/java/com/teamwiya/wiya/WiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwiya.wiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
