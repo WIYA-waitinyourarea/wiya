57d3417e92835886522b1bc3aa0b18b3903d8be0
chaeyeong222 <iky1505@naver.com>
2022-08-24 13:14:26 +0900

    test

--- src/main/java/com/teamwiya/wiya/HelloGitHub.java
+++ src/main/java/com/teamwiya/wiya/HelloGitHub.java
@@ -5,5 +5,7 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello~~");
+        System.out.println("testtest");
+
     }
 }
