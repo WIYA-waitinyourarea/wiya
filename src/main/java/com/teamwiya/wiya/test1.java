1a7660a351783d08fac3fdd83b3b952ca5d547ee
kang <gkatprkd@naver.com>
2022-08-24 13:24:07 +0900

    test1

--- src/main/java/com/teamwiya/wiya/test1.java
+++ src/main/java/com/teamwiya/wiya/test1.java
@@ -1,2 +1,7 @@
-package com.teamwiya.wiya;public class test1 {
+package com.teamwiya.wiya;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("가즈아!!!!!");
+    }
 }
