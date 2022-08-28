b45504b5738f5cec817a656652daf902c6af1d75
dooboocookie <bkh3030@gmail.com>
2022-08-28 22:13:32 +0900

    회원 엔티티 오류 수정

--- src/main/java/com/teamwiya/wiya/model/Member.java
+++ src/main/java/com/teamwiya/wiya/model/Member.java
@@ -1,20 +1,47 @@
 package com.teamwiya.wiya.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Getter @Setter
+@Getter
+@Builder
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memId;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="memId")
+    private Long id; //pk -> memId
 
-    public Member() {
+    @Column
+    private String memName;
+
+    @Column
+    private String memPwd;
+
+    @Column(unique = true)
+    private String memMail;
+
+    @Column
+    private String memNickname;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    /*
+    public static Member createMember(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder) {
+        Member member = Member.builder()
+                .memName(memberFormDTO.getMemName())
+                .memMail(memberFormDTO.getMemMail())
+                .memNickname(memberFormDTO.getMemNickname())
+                .memPwd(passwordEncoder.encode(memberFormDTO.getMemPwd()))
+                .role(MemberRole.USER)
+                .build();
+        return member;
     }
+*/
+
 }
