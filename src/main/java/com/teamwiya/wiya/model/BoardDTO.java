99fda2c9f65f2c151153837680781e65f023c069
kang <gkatprkd@naver.com>
2022-08-28 16:16:43 +0900

    kang_4_commit

---
+++ src/main/java/com/teamwiya/wiya/model/BoardDTO.java
@@ -0,0 +1,12 @@
+package com.teamwiya.wiya.model;
+
+import lombok.Getter;
+
+@Getter
+public class BoardDTO {
+
+    private Long id;
+    private String title;
+
+
+}
---
+++ src/main/java/com/teamwiya/wiya/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.teamwiya.wiya.repository;
+
+import com.teamwiya.wiya.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
