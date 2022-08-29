e0dfc51c85d0bc83d62cbfae24044fbb7a6cab7c
dooboocookie <bkh3030@gmail.com>
2022-08-29 19:41:17 +0900

    detail()_병원 정보 상세보기 Get 매핑_백경환

--- src/main/java/com/teamwiya/wiya/controller/HospitalController.java
+++ src/main/java/com/teamwiya/wiya/controller/HospitalController.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -19,29 +22,36 @@ public class HospitalController {
      * 병원 등록하는 폼을 보여주기 위한 겟방식 요청
      * @return
      */
-    @GetMapping
-    public String register_form(){
+    @GetMapping("/add")
+    public String addForm(){
         return "hospital/new-form";
     }
 
     /**
      *
      * @param hospital 뉴폼에서 넘긴 요청 파라미터들로 Hospital 객체를 만듦 > 모델에 저장
-     * @param model 뷰페이지 렌더링할떄 넘길 병원 정보를 받기 위한 모델 객체
+     * //@param model 뷰페이지 렌더링할떄 넘길 병원 정보를 받기 위한 모델 객체 >> 모델 어트리뷰트로 자동 애드
      * @return 저장한 병원의 상세페이지에 대한 논리이름을 반환
      */
-    @PostMapping
-    public String register(
-            @ModelAttribute Hospital hospital,
-            Model model){
+    @PostMapping("/add")
+    public String add(@ModelAttribute("hospital") Hospital hospital,
+                      RedirectAttributes redirectAttributes){
         Hospital savedHospital = hospitalRepository.save(hospital);
-        model.addAttribute("hospital", savedHospital);
-        // 이게 아니라 리다이렉트해야될 것 같음데..
+        redirectAttributes.addAttribute("hospitalId");
+        //model.addAttribute("hospital", savedHospital);
+        return "Redirect:/hospital/detail/{hospitalId}";
+    }
+
+    @GetMapping("/detail/{hospitalId}")
+    public String detail(@PathVariable Long hospitalId,
+                         Model model) {
+        Optional<Hospital> hospital = hospitalRepository.findById(hospitalId);
+        //여기서 널이면 예외를 발생시켜야할까??????
+        model.addAttribute("hospital", hospital.orElse(null));
         return "hospital/detail";
     }
 
-
-    @GetMapping("/{keyWord}")
+    @GetMapping("/search/{keyWord}")
     public String search(
             @PathVariable String keyWord,
             Model model){
