3fe1daca58ba7ea8158940a9566f1646e3586cd8
dooboocookie <bkh3030@gmail.com>
2022-11-25 20:33:28 +0900

    feat: swagger 설정 추가

--- src/main/java/com/teamwiya/wiya/WiyaApplication.java
+++ src/main/java/com/teamwiya/wiya/WiyaApplication.java
@@ -3,8 +3,6 @@ package com.teamwiya.wiya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.ComponentScan;
-
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
---
+++ src/main/java/com/teamwiya/wiya/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.teamwiya.wiya.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("team WIYA API")
+                        .description("위야 프로젝트 API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+}
