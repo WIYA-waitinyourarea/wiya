9e9ed7627a504fa2f2a36a4328ada5dbb8fd99bd
dooboocookie <bkh3030@gmail.com>
2022-12-06 10:29:28 +0900

    feat(hospital): 시구동 카테고리에 대해서 N+1문제 해결위해 패치 조인 적용

--- src/main/java/com/teamwiya/wiya/hospital/repository/SigudongRepository.java
+++ src/main/java/com/teamwiya/wiya/hospital/repository/SigudongRepository.java
@@ -2,11 +2,13 @@ package com.teamwiya.wiya.hospital.repository;
 
 import com.teamwiya.wiya.hospital.model.Sigudong;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class SigudongRepository {
@@ -24,11 +26,11 @@ public class SigudongRepository {
     public List<Sigudong> findAll() {
         return em.createQuery("select s from Sigudong s",Sigudong.class).getResultList();
     }
+
     public List<Sigudong> findSi() {
-        return em.createQuery("select s " +
-                        "from Sigudong s " +
-                        "where s.parent is null "
-                        , Sigudong.class)
+        return em.createQuery("select distinct si " +
+                                "from Sigudong si join fetch si.child " +
+                                "where si.parent is null ", Sigudong.class)
                 .getResultList();
     }
 
